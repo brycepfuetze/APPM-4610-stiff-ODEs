f094906bd59ed7380267a3c5e4cd550a1011fa9c
Erick White <erickwhitebusiness@gmail.com>
2025-04-11 10:47:48 -0600

    Improved RK4 demo code

--- nbody_RK4_demo.py
+++ nbody_RK4_demo.py
@@ -1,21 +1,33 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 from RungeKutta import RK4
 from EOM import nbody_EOM_rk4
 
-M = np.diag([1, 2]) # Object masses
+mu = 32930E9
 
-r = np.array([[1, 2, 3], [4, 5, 6]]) # Initial positions
-rp = np.array([[0, 0, 0], [0, 0, 0]]) # Initial velocities
+G = 6.6743E-11 # Gravitational constant
+
+M = np.diag([mu / G, 100]) # Object masses
+
+r = np.array([[0, 0, 0], [6558.9E3, 3660.8E3, -10282.0E3]]) # Initial positions
+rp = np.array([[0, 0, 0], [1.5525E3, -0.1746E3, 0.5038E3]]) # Initial velocities
 
 R0 = np.array([r, rp])
 
-G = 1 # Gravitational constant
-
 params = {'M': M, 'G': G}
 
-yapp, t = RK4(0, 1, 0.1, R0, nbody_EOM_rk4, params)
+yapp, t = RK4(0, 53054, 1E3, R0, nbody_EOM_rk4, params)
 
 yapp = yapp[:, 0, :, :]
 
-print(yapp)
+r1 = yapp[:, 0, :]
+r2 = yapp[:, 1, :]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d', aspect='equal')
+
+ax.plot(r1[:, 0], r1[:, 1], r1[:, 2])
+ax.plot(r2[:, 0], r2[:, 1], r2[:, 2])
+
+plt.show()
